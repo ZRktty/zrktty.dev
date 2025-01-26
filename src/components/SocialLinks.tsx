cf3e0a670eec0bcce4f992521fb3f16ca178865e
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2025-01-26 21:14:29 +0000

    feat(socials): add Social links to the footer

---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+interface SocialLink {
+  name: string;
+  url: string;
+  color: string;
+}
+
+const SocialLinks: React.FC = () => {
+  const [links, setLinks] = useState<SocialLink[]>([]);
+
+  useEffect(() => {
+    const fetchLinks = async () => {
+      const data = await import('@/data/socialLinks.json');
+      setLinks(data.default);
+    };
+
+    fetchLinks();
+  }, []);
+
+  return (
+    <div className="p-4 ">
+      <h5>Socials</h5>
+      <div className="flex flex-row items-center space-x-4">
+        {links.map((link) => (
+          <Link key={link.name} href={link.url} target="_blank" rel="noopener noreferrer"
+                className={`text-${link.color} hover:underline`}>
+            {link.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+
+  );
+};
+
+export default SocialLinks;
