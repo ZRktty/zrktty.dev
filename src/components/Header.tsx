e8d8bd0f7f4d954b9f488be94a239449bccf972d
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2025-01-26 22:58:57 +0000

    feat(hamburger): add menu button to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import Nav from "@/components/Nav";
 import ThemeSelector from "@/components/ThemeSelector";
 import Logo from "@/components/Logo";
+import MenuButton from "@/components/MenuButton";
 
 const Header: React.FC = () => {
   return (
     <header className="w-full p-4 flex justify-between">
-      <Logo/>
-      <Nav/>
-      <ThemeSelector/>
+      <Logo />
+      <Nav />
+      <div className='flex gap-4'>
+        <ThemeSelector />
+        <MenuButton />
+      </div>
     </header>
   );
 };
---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import * as React from "react"
+import { Menu } from "lucide-react"
+import { Button } from "@/components/ui/button"
+
+export default function MenuButton() {
+    return (
+        <Button variant="ghost" size="icon" className="md:hidden">
+            <Menu className="h-[1.2rem] w-[1.2rem]" />
+            <span className="sr-only">Open menu</span>
+        </Button>
+    )
+}
