993e6c7712ea06f5d8c5cc07db6c35f277115ffd
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2025-01-26 21:13:50 +0000

    chore: add dummy content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,21 @@
 import React from 'react';
 import LocalTime from "@/components/LocalTime";
 import SiteVersion from "@/components/SiteVersion";
+import { Separator } from "@/components/ui/separator"
+import SocialLinks from "@/components/SocialLinks";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full p-4 flex justify-between">
-      <div className="flex-1">
-        <SiteVersion/>
-      </div>
-      <div className="">
-        <LocalTime/>
-      </div>
+    <footer className="w-full p-4 ">
+      <SocialLinks/>
+      <Separator/>
+      <div className="p-4 flex justify-between">
+          <SiteVersion/>
+          <div className="text-center hidden md:block">
+            Made with ❤️ with Next.js
+          </div>
+          <LocalTime/>
+        </div>
     </footer>
   );
 };
