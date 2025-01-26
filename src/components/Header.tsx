1c42bbf937d5339a9c91e360b84c1c1d9dcfde3d
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2025-01-26 22:12:46 +0000

    feat(logo): add animated logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from "@/components/Nav";
 import ThemeSelector from "@/components/ThemeSelector";
+import Logo from "@/components/Logo";
 
 const Header: React.FC = () => {
   return (
     <header className="w-full p-4 flex justify-between">
+      <Logo/>
       <Nav/>
       <ThemeSelector/>
     </header>
