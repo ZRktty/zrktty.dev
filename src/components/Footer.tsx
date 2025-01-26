b05500f55b44ef85d5bcb68beb0156e1aaab0ad9
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2025-01-26 20:05:10 +0000

    feat(footer): add footer element; LocalTime component - show my local time

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,18 @@
+"use client"
+
 import React from 'react';
+import LocalTime from "@/components/LocalTime";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full p-4 text-white">
-      {/* Add your footer content here */}
-      Copyright © 2025 | Zoltan Rakottyai
+    <footer className="w-full p-4 flex justify-between">
+      <div className="flex-1">
+        {/* Add content for the first column here */}
+        Column 1 content
+      </div>
+      <div className="">
+        <LocalTime/>
+      </div>
     </footer>
   );
 };
---
+++ src/components/LocalTime.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+
+const LocalTime: React.FC = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedTime = new Intl.DateTimeFormat(navigator.language, {
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZone: 'Atlantic/Canary',
+    timeZoneName: 'short',
+  }).format(currentTime);
+
+  return (
+    <div>
+      <h5>Local time</h5>
+      <div>{formattedTime}</div>
+    </div>
+  );
+};
+
+export default LocalTime;
