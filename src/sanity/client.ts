2194dd537f40f6654699e8a1e889743cd4629767
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2025-01-31 20:20:58 +0000

    chore(sanity): use env var for projectID

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "8tbsip27",
+  projectId: process.env.SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false,
