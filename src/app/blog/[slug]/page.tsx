2376370a92809b137f62b8cd80d4c001966b7477
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2025-01-31 20:10:11 +0000

    feat(cms): add blog and post page boilerplate components

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BlogPostPage = () => {
+  return (
+    <div>
+      <h1>Post title</h1>
+      <p>Post content</p>
+    </div>
+  );
+};
+
+export default BlogPostPage;
