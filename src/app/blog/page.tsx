b3b48aeae727202f22d5e4635a1032bd0f68fb3d
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2025-01-31 20:17:38 +0000

    fix(blog): link to single post

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -18,7 +18,7 @@ export default async function BlogPage() {
       <ul className="flex flex-col gap-y-4">
         {posts.map((post) => (
           <li className="hover:underline" key={post._id}>
-            <Link href={`/${post.slug.current}`}>
+            <Link href={`/blog/${post.slug.current}`}>
               <h2 className="text-xl font-semibold">{post.title}</h2>
               <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
             </Link>
