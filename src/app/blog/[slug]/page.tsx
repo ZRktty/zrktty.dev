7c78e826118a454603fc3f7255944f3747f2aacd
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2025-02-02 00:43:46 +0000

    chore(Sanity): extract Sanity image handling function to src/sanity/utils.ts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,23 +1,16 @@
 import { PortableText, type SanityDocument } from "next-sanity";
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { client } from "@/sanity/client";
 import Link from "next/link";
 import Image  from "next/image";
+import { urlFor } from "@/sanity/utils";
 
 const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
-
-const { projectId, dataset } = client.config();
-const urlFor = (source: SanityImageSource) =>
-  projectId && dataset
-    ? imageUrlBuilder({ projectId, dataset }).image(source)
-    : null;
 
 const options = { next: { revalidate: 30 } };
 
 export default async function PostPage({
-                                         params,
-                                       }: {
+   params,
+}: {
   params: Promise<{ slug: string }>;
 }) {
   const post = await client.fetch<SanityDocument>(POST_QUERY, await params, options);
