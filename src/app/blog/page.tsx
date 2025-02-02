f398c448d0c62d0333b03ac3c476e6472860e323
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2025-02-02 00:46:35 +0000

    feat(blog): add PostItem component - renders content excerpt (max 255char) - renders post cover image

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,28 +1,35 @@
-import Link from "next/link";
-import { type SanityDocument } from "next-sanity";
-
-import { client } from "@/sanity/client";
+import PostItem from "@/components/Blog/PostItem";
+import {client} from "@/sanity/client";
+import {Post} from "@/sanity/types";
+import React from "react";
 
 const POSTS_QUERY = `*[
-  _type == "post"
+  _type == "post" 
   && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
+] | order(publishedAt desc)[0...12] {
+  _id,
+  title,
+  slug,
+  publishedAt,
+  mainImage,
+  "excerpt": array::join(
+    string::split(pt::text(body), "")[0..255], 
+    ""
+  ) + "..."
+}`;
 
-const options = { next: { revalidate: 30 } };
+const options = {next: {revalidate: 30}};
 export default async function BlogPage() {
-  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+  // @TODO: Implement pagination
+  // @TODO: figure out how to generate types for the query
+  const posts = await client.fetch<Post[]>(POSTS_QUERY, {}, options);
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8">
       <h1 className="text-4xl font-bold mb-8">Posts</h1>
-      <ul className="flex flex-col gap-y-4">
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {posts.map((post) => (
-          <li className="hover:underline" key={post._id}>
-            <Link href={`/blog/${post.slug.current}`}>
-              <h2 className="text-xl font-semibold">{post.title}</h2>
-              <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
-            </Link>
-          </li>
+          <PostItem post={post} key={post._id}/>
         ))}
       </ul>
     </main>
