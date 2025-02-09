abb09c016ba6e16ce94fa07961ba89049fd43409
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2025-02-09 22:04:39 +0000

    feat(Blog): added view when no posts available

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,6 @@
-import PostItem from "@/components/Blog/PostItem";
+import PostItem, {PostWithExcerpt} from "@/components/Blog/PostItem";
 import {client} from "@/sanity/client";
-import { POSTS_QUERY } from "@/sanity/queries";
+import {POSTS_QUERY} from "@/sanity/queries";
 import {Post} from "@/sanity/types";
 
 
@@ -8,15 +8,20 @@ const options = {next: {revalidate: 30}};
 export default async function BlogPage() {
   // @TODO: Implement pagination
   // @TODO: figure out how to generate types for the query
-  const posts = await client.fetch<Post[]>(POSTS_QUERY, {}, options);
+  const posts = await client.fetch<Post[]>(POSTS_QUERY, {}, options) as PostWithExcerpt[];
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8">
       <h1 className="text-4xl font-bold mb-8">Posts</h1>
       <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {posts.map((post) => (
-          <PostItem post={post} key={post._id}/>
-        ))}
+        {
+          posts.length > 0 ? (
+            posts.map((post) => (
+              <PostItem post={post} key={post._id}/>
+            ))) : (
+            <p>No posts found :(</p>
+          )
+        }
       </ul>
     </main>
   );
